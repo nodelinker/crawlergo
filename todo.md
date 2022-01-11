9a3a94bef96859f77dfd8f52449f178fda727e8b
unknown <nodelinker@gmail.com>
2022-01-11 10:25:28 +0800

    blablablabla

--- todo.md
+++ todo.md
@@ -2,3 +2,4 @@
 
 * 改造，单个tab页，解析所有link包括ajax click等操作。这里面的问题是如何保证ajax或js dom这类的异步操作能够同步得到结果后再退出（理想状态）。
 * chrome 通过intercept截获的动态连接，需要记录其content-type只有html页面才需要新打开一个tab进行爬取。
+* 提交表单指向内部的iframe时，后面js click点击会出一些奇怪的bug。
--- xxxjs/Untitled-2.js
+++ xxxjs/Untitled-2.js
@@ -4,9 +4,13 @@ function sleep(ms) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
+function syncSleep(sleepTime) {
+	for(var start = new Date; new Date - start <= sleepTime;) {}
+}
+
 var MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
 var observeDOM = (function () {
-    return function (obj, callback) {
+  return function (obj, callback) {
     if (!obj || obj.nodeType !== 1) return;
 
     if (MutationObserver) {
@@ -38,8 +42,8 @@ observer = observeDOM(listElm, async function (m) {
     (record) =>
       record.removedNodes.length & removedNodes.push(...record.removedNodes)
   );
-  console.clear();
-  console.log("Added:", addedNodes, "Removed:", removedNodes);
+  // console.clear();
+  // console.log("Added:", addedNodes, "Removed:", removedNodes);
 
   for (var i = 0; i < addedNodes.length; i++) {
     try {
@@ -51,16 +55,27 @@ observer = observeDOM(listElm, async function (m) {
       for (let node1 of nodeListHref) {
         let attrValue = node1.getAttribute("href");
         if (attrValue.toLocaleLowerCase().startsWith("javascript:")) {
-          await sleep(1000);
+          // await sleep(1000);
           eval(attrValue.substring(11));
+        }
+        for (var i = 0;i < 10; i++){
+          if (window.globalLock  == 1){
+            await sleep(1000);
+          }
         }
       }
       let nodeListSrc = _.querySelectorAll("[src]");
       for (let node2 of nodeListSrc) {
         let attrValue = node2.getAttribute("src");
         if (attrValue.toLocaleLowerCase().startsWith("javascript:")) {
-          await sleep(1000);
+          // await sleep(1000);
           eval(attrValue.substring(11));
+        }
+
+        for (var i = 0;i < 10; i++){
+          if (window.globalLock  == 1){
+            await sleep(1000);
+          }
         }
       }
     } catch (error) {
@@ -70,29 +85,112 @@ observer = observeDOM(listElm, async function (m) {
   }
 });
 
+var origOpen = XMLHttpRequest.prototype.open;
+XMLHttpRequest.prototype.open = function () {
 
-(async function click_all_a_tag_javascript(){
-	let nodeListHref = document.querySelectorAll("[href]");
-	for (let node of nodeListHref) {
-		let attrValue = node.getAttribute("href");
-		if (attrValue.toLocaleLowerCase().startsWith("javascript:")) {
+  console.log("request started!");
+  window.globalLock = 1;
+
+  // debugger
+  console.log(arguments);
+  this.addEventListener("load", function () {
+    console.log("request completed!");
+    // console.log(this.readyState);
+    // console.log(this.responseText); //whatever the response was
+    window.globalLock = 0;
+  });
+  this.addEventListener("abort", function () {
+    console.log("request abord!");
+    // console.log(this.readyState);
+    // console.log(this.responseText); //whatever the response was
+  });
+
+  // this.addEventListener("timeout", function () {
+  //   console.log("request completed!");
+  //   console.log(this.readyState);
+  //   console.log(this.responseText); //whatever the response was
+  // });
+  origOpen.apply(this, arguments);
+};
+
+
+let origSend = XMLHttpRequest.prototype.send;
+XMLHttpRequest.prototype.send = function() {
+  
+  console.log("request send!");
+
+   this.addEventListener('readystatechange', function() {
+      // if (this.responseURL.includes(urlmatch) && this.readyState === 4) {
+      //    callback(this);
+      // }
+      console.log(arguments);
+   }, false);
+   origSend.apply(this, arguments);
+};
+
+
+(async function click_all_a_tag_javascript() {
+  let nodeListHref = document.querySelectorAll("[href]");
+  for (let node of nodeListHref) {
+    let attrValue = node.getAttribute("href");
+    if (attrValue.toLocaleLowerCase().startsWith("javascript:")) {
+      try {
+        eval(attrValue.substring(11));
+
+      } catch {}
+    }
+
+    for (var i = 0;i < 10; i++){
+      console.log("aaaaaaaaaaaaaaaaaaaaaaaa", i);
+      if (window.globalLock  == 1){
+        await sleep(1000);
+      }
+    }
+  }
+  let nodeListSrc = document.querySelectorAll("[src]");
+  for (let node of nodeListSrc) {
+    let attrValue = node.getAttribute("src");
+    if (attrValue.toLocaleLowerCase().startsWith("javascript:")) {
+      try {
+        eval(attrValue.substring(11));
+        await sleep(1000);
+      } catch {}
+    }
+
+    for (var i = 0;i < 10; i++){
+      console.log("bbbbbbbbbbbbbbbbbbbbbbbbb", i);
+      if (window.globalLock  == 1){
+        await sleep(1000);
+      }
+    }
+    
+  }
+})();
+
+
+(async function trigger_all_inline_event(){
+	let eventNames = ["onabort", "onblur", "onchange", "onclick", "ondblclick", "onerror", "onfocus", "onkeydown", "onkeypress", "onkeyup", "onload", "onmousedown", "onmousemove", "onmouseout", "onmouseover", "onmouseup", "onreset", "onresize", "onselect", "onsubmit", "onunload"];
+	for (let eventName of eventNames) {
+		let event = eventName.replace("on", "");
+		let nodeList = document.querySelectorAll("[" + eventName + "]");
+		if (nodeList.length > 100) {
+			nodeList = nodeList.slice(0, 100);
+		}
+		for (let node of nodeList) {
 			await sleep(1000);
+			let evt = document.createEvent('CustomEvent');
+			evt.initCustomEvent(event, false, true, null);
 			try {
-				eval(attrValue.substring(11));
+				node.dispatchEvent(evt);
 			}
 			catch {}
-		}
-	}
-	let nodeListSrc = document.querySelectorAll("[src]");
-	for (let node of nodeListSrc) {
-		let attrValue = node.getAttribute("src");
-		if (attrValue.toLocaleLowerCase().startsWith("javascript:")) {
-			await sleep(1000);
-			try {
-				eval(attrValue.substring(11));
-			}
-			catch {}
-		}
-	}
 
+      for (var i = 0;i < 10; i++){
+        console.log("bbbbbbbbbbbbbbbbbbbbbbbbb", i);
+        if (window.globalLock  == 1){
+          await sleep(1000);
+        }
+      }
+		}
+	}
 })()
